479646acf20d5fd1797ccb8140715f0fc8e74af1
Valentina <deminav2295@gmail.com>
2020-09-01 10:26:50 +0300

    урок react элементы

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { result } from './number';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-        </p>
-				<p style={{
-					color: 'red',
-					fontSize: 40
-				}}> Hello World!
-				</p>
-				<p>
-					{result}
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-        </a>
-			</header>
-		</div>
-	);
-}
-export default App;
+const element = (<div>
+	Hello World!
+	<p>My name is Valya^^</p>
+</div>)
+
+export { element };
--- src/number.js
+++
@@ -1,5 +0,0 @@
-const count = 12;
-const length = 15;
-const result = count * length;
-
-export { result }; 
