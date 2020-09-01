62e503041766bd38c1ebf3c7b770d924477d5579
Valentina <deminav2295@gmail.com>
2020-09-01 11:36:40 +0300

    урок подключение стилей

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const todoItem = 'Написать новое приложение'
 
@@ -19,12 +20,12 @@ const App = () => {
 	];
 
 	return (
-		<div>
-			<h1>Важные дела:</h1>
+		<div className="wrap">
+			<h1 className="wrap__title">Важные дела:</h1>
 			<InputItem />
 			<ItemList items={items} />
 			<Footer count={3} />
-		</div>);
+		</div >);
 }
 
 export default App;
