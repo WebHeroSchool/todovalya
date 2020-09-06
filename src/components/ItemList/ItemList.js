b0e31bfc939d36945a105f5c34cd16ef555f55c1
Valentina <deminav2295@gmail.com>
2020-09-06 07:44:10 +0300

    урок prop-types

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -27,10 +28,11 @@ const ItemList = ({ items, onClickDone, onClickDelete }) => (<ul>
 		</li>)}
 </ul>);
 
-ItemList.defaultProps = {
-	items: {
-		value: "Важные дела отсутствуют"
-	}
+ItemList.propTypes = {
+	items: PropTypes.array.isRequired,
+	isDone: PropTypes.bool,
+	onClickDone: PropTypes.func,
+	onClickDelete: PropTypes.func
 };
 
 export default ItemList
