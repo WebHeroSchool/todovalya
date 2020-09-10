d0b9a709ef9b9cb997de401ff5d66c09b6b79f13
Valentina <deminav2295@gmail.com>
2020-09-10 08:01:13 +0300

    react router

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+
+const Contacts = () => (
+	<div className={styles.wrap}>
+		<h1>Моя почта: deminav@gmail.com</h1>
+	</div>
+);
+
+export default Contacts;
